29594e2326b7023b80b043d25bd05cc3a4abd955
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-16 02:26:18 +0000

    creating the server, setting up template engine, and testing

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const io = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 let app = express();
 let server = http.Server(app);
 
+// serving static files
+
+app.use(express.static(path.join(__dirname,'public')));
+
+// templating engine setup
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname, 'views'));
+
 // test server route
 app.get('/', (req, res, next) => {
-  res.send('I am working!');
+  res.render('index');
 });
 
 
